180f11442d6a650aa28b8d84be7e4f526fc49bd4
Luis Zuniga <luiszu7779@gmail.com>
2025-01-09 13:48:29 +0100

    Clean

--- lib/map/tiles_provider.dart
+++ lib/map/tiles_provider.dart
@@ -2,9 +2,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 
 TileLayer get openStreetMapTileLayer => TileLayer(
-      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-      userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-      // Use the recommended flutter_map_cancellable_tile_provider package to
-      // support the cancellation of loading tiles.
-      tileProvider: CancellableNetworkTileProvider(),
-    );
+  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+  userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+  tileProvider: CancellableNetworkTileProvider(),
+);
